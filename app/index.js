3b3dcc642df06ee08e002d447a987238d788fc56
Yuya Saito <studiomohawk@gmail.com>
2017-01-17 09:53:29 +0900

    LGTM

--- app/index.js
+++ app/index.js
@@ -1,10 +1,1 @@
 import './index.scss';
-
-import Stripe from './components/stripe/';
-
-const stripe = new Stripe('.if-c-stripe');
-
-window.setInterval(() => {
-  stripe.rotate(1);
-  stripe.render();
-}, 20);
