7bb6e4d1895ebdd77bf17e733c66f8f7acf6c335
hiloki <service@inkdesign.jp>
2019-04-17 15:44:50 +0900

    Add Speaker details

--- dist/speakers.353e354c.js
+++ dist/speakers.353e354c.js
@@ -171,7 +171,24 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"node_modules/reset-css/reset.css":[function(require,module,exports) {
+
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/app.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./assets/1.jpg":[["1.9f6060a0.jpg","src/assets/1.jpg"],"src/assets/1.jpg"],"./assets/halftone-light.png":[["halftone-light.e1ed191e.png","src/assets/halftone-light.png"],"src/assets/halftone-light.png"],"./assets/4.jpg":[["4.d9e45535.jpg","src/assets/4.jpg"],"src/assets/4.jpg"],"./assets/3.jpg":[["3.9d3bc06a.jpg","src/assets/3.jpg"],"src/assets/3.jpg"],"./assets/abematowers.jpg":[["abematowers.7abc3762.jpg","src/assets/abematowers.jpg"],"src/assets/abematowers.jpg"],"./assets/2.jpg":[["2.d652021f.jpg","src/assets/2.jpg"],"src/assets/2.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"src/speakers.js":[function(require,module,exports) {
+"use strict";
+
+require("reset-css");
+
+require("./app.css");
+},{"reset-css":"node_modules/reset-css/reset.css","./app.css":"src/app.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -340,4 +357,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/speakers.js"], null)
+//# sourceMappingURL=/speakers.353e354c.map
