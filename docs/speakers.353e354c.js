a74a064a3f83a8cfcad194cee9f4dc1457dc8c58
hiloki <service@inkdesign.jp>
2019-04-18 01:11:06 +0900

    build

--- docs/speakers.353e354c.js
+++ docs/speakers.353e354c.js
@@ -215,7 +215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51203" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59772" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
---
+++ docs/speakers.a057d3c8.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRequire,o="function"==typeof require&&require;function u(n,t){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!t&&f)return f(n,!0);if(i)return i(n,!0);if(o&&"string"==typeof n)return o(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[n][1][r]||r},p.cache={};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,p,l,l.exports,this)}return r[n].exports;function p(e){return u(p.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=i,u.register=function(r,n){e[r]=[function(e,r){r.exports=n},{}]};for(var f=0;f<n.length;f++)u(n[f]);if(n.length){var c=u(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):t&&(this[t]=c)}return u}({"szrR":[function(require,module,exports) {
+
+},{}],"WjnD":[function(require,module,exports) {
+
+},{"./assets/1.jpg":[["1.577b3b2c.jpg","dEDz"],"dEDz"],"./assets/halftone-light.png":[["halftone-light.02bed7fe.png","e3jN"],"e3jN"],"./assets/4.jpg":[["4.5726eaa2.jpg","psXp"],"psXp"],"./assets/3.jpg":[["3.095f8ea1.jpg","2IqD"],"2IqD"],"./assets/abematowers.jpg":[["abematowers.96eb4b69.jpg","NNUK"],"NNUK"],"./assets/7.jpg":[["7.7450bdb5.jpg","typ2"],"typ2"]}],"thLQ":[function(require,module,exports) {
+"use strict";require("reset-css"),require("./app.css");
+},{"reset-css":"szrR","./app.css":"WjnD"}]},{},["thLQ"], null)
+//# sourceMappingURL=/speakers.a057d3c8.map
